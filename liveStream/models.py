f011acca73b119386f008e57f4a7df48c09bfa80
MVS Sriharsha <sriharshamvs@gmail.com>
2020-05-07 17:11:26 +0530

    added models

--- liveStream/models.py
+++ liveStream/models.py
@@ -1,3 +1,12 @@
+from django.core.files.storage import FileSystemStorage
 from django.db import models
 
 # Create your models here.
+fs = FileSystemStorage(location='/media/photos')
+class LiveStreamTheme(models.Model):
+    stream_key = models.CharField(max_length=50)
+    stream_url = models.CharField(max_length=50)
+    comments_server = models.CharField(max_length=50)
+    comments_room = models.CharField(max_length=50)
+    img_src = models.ImageField(upload_to=fs)
+    description = models.TextField()
