8172d1476f38193e688f4a1ef32a78a23f5ca72a
= <=>
2020-12-18 12:00:03 +0530

    Created a new folder (ui-redesign) and added the files

---
+++ liveStream/templates/liveStream/ui-redesign/app.js
@@ -0,0 +1,37 @@
+// FOr smooth transition between light and dark theme
+
+const themeButton = document.querySelector('.theme-toggle');
+
+let currentTheme = localStorage.getItem('data-theme');
+console.log(currentTheme);
+if (!currentTheme) {
+    localStorage.setItem('data-theme', 'light');
+}
+
+applyTheme(currentTheme);
+
+themeButton.addEventListener('click', e => {
+    if (document.documentElement.getAttribute('data-theme') === 'light') {
+        applyTheme("dark");
+        localStorage.setItem('data-theme', 'dark');
+    } else {
+        localStorage.setItem('data-theme', 'light');
+        applyTheme('light');
+    }
+    transition();
+});
+
+function applyTheme(theme) {
+    if (theme === 'light') {
+        document.documentElement.setAttribute('data-theme', 'light');
+    } else {
+        document.documentElement.setAttribute('data-theme', 'dark')
+    }
+}
+
+function transition() {
+    document.documentElement.classList.add('smooth');
+    window.setTimeout(() => {
+        document.documentElement.classList.remove('smooth');
+    }, 1000);
+}
