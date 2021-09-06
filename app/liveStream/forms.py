5aa9a824fee71c8aca976e7b463e859f0046c934
sriharshamvs <sriharshamvs@gmail.com>
2021-09-06 10:43:51 +0530

    feat: added form for FDP sessions

--- app/liveStream/forms.py
+++ app/liveStream/forms.py
@@ -1,10 +1,15 @@
 from django.db import models
 from django.forms import ModelForm, Form
-from .models import ContactDetail
+from .models import ContactDetail, ContactDetailFDP
 
 class ContactDetailsForm(ModelForm):
     class Meta:
         model = ContactDetail
         fields = ['name','mobile_number','email_address','place']
-
         labels = {"place":"Institution"}
+
+class ContactDetailFdpForm(ModelForm):
+    class Meta:
+        model = ContactDetailFDP
+        fields = ['name', 'mobile_number', 'email_address', 'unique_id', 'department', \
+         'institute', 'state_of_the_institute', 'country_of_the_institute']
--- app/liveStream/views.py
+++ app/liveStream/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
-from .models import LiveStream, ContactDetail
-from .forms import ContactDetailsForm 
+from .models import LiveStream, ContactDetail, ContactDetailFDP
+from .forms import ContactDetailsForm, ContactDetailFdpForm 
 import csv
 
 # Create your views here.
@@ -14,14 +14,24 @@ def export(request, stream_key):
         return redirect('/admin')
 
     l = LiveStream.objects.filter(stream_key = stream_key)[0]
-    contacts = ContactDetail.objects.filter(live_stream=l).values_list('name','mobile_number','email_address','place').distinct()
     response = HttpResponse(content_type='text/csv')
     response['Content-Disposition'] = f'attachment; filename="{stream_key}.csv"'
+    if l.collect_details:
+        contacts = ContactDetail.objects.filter(live_stream=l).values_list('name','mobile_number','email_address','place').distinct()
+        writer = csv.writer(response)
+        writer.writerow(['Name', 'Mobile Number', 'Email Address', 'Institution/Place'])
+        for cm in contacts:
+            writer.writerow(cm)
+    elif l.fdp_details:
+        contacts = ContactDetailFDP.objects.filter(live_stream=l).values_list('name', 'mobile_number', 'email_address', 'unique_id', 'department', \
+         'institute', 'state_of_the_institute', 'country_of_the_institute').distinct()
+        writer = csv.writer(response)
+        writer.writerow(['name', 'mobile_number', 'email_address', 'unique_id', 'department', \
+         'institute', 'state_of_the_institute', 'country_of_the_institute'])
+        for cm in contacts:
+            writer.writerow(cm)
 
-    writer = csv.writer(response)
-    writer.writerow(['Name', 'Mobile Number', 'Email Address', 'Institution/Place'])
-    for cm in contacts:
-        writer.writerow(cm)
+    
 
     return response
 
@@ -45,6 +55,28 @@ def room(request, stream_key):
 
             elif request.method == 'POST':
                 f = ContactDetailsForm(request.POST)
+                if f.is_valid():
+                    c = f.save(commit=False)
+                    c.live_stream = stream
+                    c.save()
+                    request.session['contact_id'] = c.id
+                    return render(request, 'liveStream/room.html', {'stream': stream, 'contact':c})
+                else:
+                    return render(request, 'liveStream/register.html', {'stream':stream, 'form':f})
+        elif stream.fdp_details:
+            cookie = None
+            if 'contact_id' in request.session:
+                cookie = request.session['contact_id']
+            if request.method == 'GET':
+                if not cookie:
+                    f = ContactDetailFdpForm()
+                    return render(request, 'liveStream/register.html', {'stream':stream, 'form':f})
+                else:
+                    contact = ContactDetail.objects.filter(id=cookie)[0]
+                    return render(request, 'liveStream/room.html', {'stream': stream, 'contact':contact})
+
+            elif request.method == 'POST':
+                f = ContactDetailFdpForm(request.POST)
                 if f.is_valid():
                     c = f.save(commit=False)
                     c.live_stream = stream
